c3c6533e0dce4016e2dd82ff5261a5361b97cb5f
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2025-12-28 20:08:12 +0530

    Implement GCD and LCM functions
    Added functions to calculate GCD and LCM of two numbers.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -83,4 +83,19 @@ def gcd(a, b):
 a = 98
 b = 56
 print(f"GCD of {a} and {b} is {gcd(a, b)}")# 14
-=======
+======= LCM Of Two Numbers========================================
+# GCD and HCF of two numbers
+# Efficient
+
+def gcd(a,b):
+    
+    if b == 0 :
+        return a
+    return gcd(b, a % b)
+    
+def lcm(a,b) :
+    return a * b // gcd(a,b)
+
+a = 12
+b = 15
+print(lcm(a,b))#60
