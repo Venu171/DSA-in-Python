c7afa1f1dec240c7ac9c8a9dd85af31a4becf0d8
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2025-12-29 21:17:22 +0530

    Implement prime checking function
    Added an efficient function to check for prime numbers.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -99,3 +99,28 @@ def lcm(a,b) :
 a = 12
 b = 15
 print(lcm(a,b))#60
+
+=============================Check for prime======================
+# Efficient
+
+def isprime(n) :
+    if n == 1 :
+        return False
+        
+    if n == 2 or n == 3 :
+        return True
+        
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+        
+    i = 5
+    while (i * i <= n) :
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+        
+    return True
+    
+
+n = 7
+print(isprime(n))
