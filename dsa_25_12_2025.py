3fb4e034b000940c73c1595954c8b037ef176368
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2025-12-30 20:44:34 +0530

    Implement prime checking and factorization functions
    Added functions to check for prime numbers and to compute prime factors.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -124,3 +124,19 @@ def isprime(n) :
 
 n = 7
 print(isprime(n))
+=====================Prime Factorization======================
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def prime_factors(n):
+    for i in range(2, n + 1):
+        if is_prime(i):
+            while n % i == 0:
+                print(i, end=' ')
+                n //= i
+prime_factors(100)#2 2 5 5
