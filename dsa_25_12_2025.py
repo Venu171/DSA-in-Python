340def50b02b4ec6b28d022733b04c46884e7fd2
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2025-12-31 22:54:53 +0530

    Implement inverted right-angled triangle function
    Added function to generate inverted right-angled triangle pattern.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -203,3 +203,17 @@ def right_angled_triangle(n):
         res_arr.append("*"*i)
     return res_arr
 print(right_angled_triangle(5))#['*', '**', '***', '****', '*****']
+#======================================Inverted Right Angled Triangle====================================
+"""
+You are given an integer n. 
+Your task is to return an inverted right-angled triangle pattern of '*' 
+where each side has n characters, represented as a list of strings. 
+The first row should have n stars, the second row n-1 stars, and so on, 
+until the last row has 1 star.
+"""
+def inverted_right_angled_triangle(n):
+    res_arr=[]
+    for i in range(n,0,-1):
+        res_arr.append("*"*i)
+    return res_arr
+print(inverted_right_angled_triangle(5))#['*****', '****', '***', '**', '*']
