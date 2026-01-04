c6d1a24815341d1c2309fbd607da4634efe8d8b5
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-04 10:44:38 +0530

    Implement recursive power function
    Added a function to compute power using recursion.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -320,6 +320,18 @@ def SieveOfEratosthenes(num):
 			print(p)
 SieveOfEratosthenes(20)
 # 2 3 5 7 11 13 17 19
+#================================Computing Power==================================
+def power(x, y):
+	temp = 0
+	if(y == 0):
+		return 1
+	temp = power(x, int(y / 2))
+	if (y % 2 == 0) :
+	    return temp * temp
+	else :
+	    return x * temp * temp
+
+print(power(2,3))
 #================================Diamond Pattern========================================
 """
 Problem Description:
