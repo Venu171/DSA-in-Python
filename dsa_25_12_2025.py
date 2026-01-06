382d83025a95dfc915d8ab292d63de9d447a594e
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-06 13:24:39 +0530

    Implement iterative power function
    Added an iterative power function using binary exponentiation.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -370,3 +370,20 @@ diamond = generate_diamond(size)
 # ['   *', '  ***', ' *****', '*******', ' *****', '  ***', '   *']
 print("\nList representation:")
 print(diamond)
+#=============================Iterative Power (Binary Exponentiation)==================
+def power(x,n) :
+    
+    res = 1 
+    
+    while n > 0 :
+        if n % 2 != 0 :
+            res = res * x 
+        x = x * x
+        n = n // 2 
+    
+    return res 
+    
+
+x = 4
+n = 5
+print(power(x,n))#1024###(4**5=10124)
