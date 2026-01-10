b965769af8ffe2811f8771991c74a312ca2f79e8
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-10 12:48:41 +0530

    Implement function to get second largest element
    Added a function to find the second largest element in a list.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -435,3 +435,32 @@ def getMax(l):
 
 l = [1, 2, 3, 4, 5]
 print(getMax(l))  # 5
+#==========================Second Largest Element in a List===============================
+def getMax(l):
+    if not l:
+        return None
+    else:
+
+        res = l[0]                  # assume l[0] is the max
+        for i in range(1, len(l)):  # iterate through index 1 to last
+            if l[i] > res:           # check whether current element is greater than res
+                res = l[i]          # if current element is greater than res ,update res to current
+        return res
+    
+def getSecMax(l):
+    if len(l) <= 1:
+        return None
+    lar = getMax(l)
+    slar = None
+
+    for x in l:
+
+        if x != lar:
+            if slar == None:
+                slar = x
+            else:
+                slar = max(x, slar)
+    return slar
+
+l = [int(x) for x in input().split()]
+print(getSecMax(l))#34
