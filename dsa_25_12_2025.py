7a56ed53c780b95bc5805605af95c4a7b5f0d731
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-15 18:54:37 +0530

    Implement palindrome check using recursion
    Added recursive functions to check for palindrome.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -570,7 +570,24 @@ def dSum(n):
     if n < 10:
         return n
     return dSum(n // 10) + n % 10
-
-
-
 print(dSum(253))#10
+#====================Palindrome check using Recursion===================================
+def isPalRec(st, s, e) :	
+	if (s == e):
+		return True
+	if (st[s] != st[e]) :
+		return False
+	if (s < e + 1) :
+		return isPalRec(st, s + 1, e - 1);
+	return True
+
+def isPalindrome(st) :
+	n = len(st)
+	if (n == 0) :
+		return True
+	return isPalRec(st, 0, n - 1);
+st = "geeg"
+if (isPalindrome(st)) :
+	print("Yes")
+else :
+	print("No")
