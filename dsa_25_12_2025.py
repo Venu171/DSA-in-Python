68c107156fa89cee2cc50ff6d55453b9f064344a
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-17 16:28:37 +0530

    Implement linear search algorithm
    Added a linear search function to find an element in an array.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -619,3 +619,17 @@ def floorSqrt(x):
 	return ans
 x = 11
 print(floorSqrt(x))#3
+#=====================Linear Search==========================================================
+def search(arr, n, x):
+    for i in range(0, n):
+      if (arr[i] == x):
+        return i
+	return -1
+arr = [2, 3, 4, 10, 40]
+x = 10
+n = len(arr)
+result = search(arr, n, x)
+if (result == -1):
+    print("Element is not present in array")
+else :
+    print("Element is present at index", result)
