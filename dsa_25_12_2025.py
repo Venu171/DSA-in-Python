962f3f7f0f49900f0759577cff6dcd76f43a8dcf
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-18 09:09:23 +0530

    Implement count_occurrences function
    Added functions to count occurrences of an element in a sorted array.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -633,3 +633,34 @@ if (result == -1):
     print("Element is not present in array")
 else :
     print("Element is present at index", result)
+#==================Count Occurrences in Sorted Array===========================================
+def search(nums, target, find_start_index):
+    answer = -1
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if target < nums[mid]:
+            end = mid - 1
+        elif target > nums[mid]:
+            start = mid + 1
+        else:
+            answer = mid
+            if find_start_index:
+                end = mid - 1
+            else:
+                start = mid + 1
+    return answer
+
+def count_occurrences(nums, x):
+    first_index = search(nums, x, True)
+    last_index = search(nums, x, False)
+    if first_index != -1 and last_index != -1:
+        return last_index - first_index + 1
+    else:
+        return 0
+
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
+x = 2
+c = count_occurrences(arr, x)
+print(c)#4
