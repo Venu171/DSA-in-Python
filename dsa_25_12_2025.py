aa652e7a7b54ddbaeffc0766bbba39d6e3a0dfd0
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-19 12:04:29 +0530

    Implement minDiff function for chocolate distribution
    Added function to solve the Chocolate Distribution Problem.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -664,3 +664,17 @@ arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
 x = 2
 c = count_occurrences(arr, x)
 print(c)#4
+#===================Chocolate Distribution Problem===============================
+def minDiff(arr,m) :
+    if (m == 0 or len(arr) == 0) :
+        return 0
+    if (len(arr)<m) :
+        return -1
+    arr.sort()
+    res = arr[m-1] - arr[0]
+    for i in range(1, len(arr)- m+1) :
+        res = min(res,abs(arr[i + m-1] - arr[i]))
+    print(res)
+    
+arr = [7,3,1,8,9,12,56]
+minDiff(arr,3)#2
