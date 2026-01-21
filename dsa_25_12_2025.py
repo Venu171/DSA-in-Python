feb779ad0640aa4b9c994f2ec2a305351132217c
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-21 09:39:46 +0530

    Add Kth smallest element function
    Implement Kth smallest element function using partitioning.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -709,3 +709,31 @@ def cyclesortdist(arr) :
 arr = [20,40,30,10,50]
 n = len(arr)
 print(cyclesortdist(arr))#[10,20,30,40,50]
+#====================Kth Smallest=============================
+def partition(arr, l, r):
+ 
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+        if (arr[j] <= x):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+def kthsmallest(arr, k) :
+    l = 0 
+    r = len(arr) - 1 
+    while l <= r :
+        p = partition(arr,l,r) 
+        if p == k-1 :
+            return p 
+                
+        elif p > k-1 :
+            r = p - 1 
+                
+        else :
+            l = p + 1 
+    return -1
+arr = [10,4,5,8,6,26]
+k = 5 
+print(kthsmallest(arr, k))#4
