4396b49081916581fb62e700d074679b1f710c0c
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-25 10:40:02 +0530

    Implement linked list traversal functionality
    Added a Node class and printList function to traverse and print linked list elements.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -815,3 +815,22 @@ head = temp1
 print(head.key)#10
 print(head.next.key)#20
 print(head.next.next.key)#30
+#=================Traversal of a Linked List=======================
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+
+def printList(head):
+    curr = head
+    while curr!= None:
+        print(curr.key)
+        curr = curr.next
+
+
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(15)
+head.next.next.next = Node(30)
+printList(head)# 10 20 15 30
