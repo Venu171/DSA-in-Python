d6250ef202df721db3333676a5ba601543220e31
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-26 09:41:07 +0530

    Implement search function in linked list
    Added a search function to find the position of a value in the linked list.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -820,17 +820,36 @@ class Node:
     def __init__(self, key):
         self.key = key
         self.next = None
-
-
 def printList(head):
     curr = head
     while curr!= None:
         print(curr.key)
         curr = curr.next
 
-
 head = Node(10)
 head.next = Node(20)
 head.next.next = Node(15)
 head.next.next.next = Node(30)
 printList(head)# 10 20 15 30
+#===================Search in Linked List======================================
+class Node:
+    def __init__(self, k):
+        self.key = k
+        self.next = None
+
+def search(head, x):
+    pos = 1
+    curr = head
+    while curr != None:
+        if curr.key == x:
+            return pos
+        pos = pos + 1
+        curr = curr.next
+    return -1
+
+head = Node(10)
+head.next = Node(15)
+head.next.next = Node(20)
+head.next.next.next = Node(25)
+x = 20
+print(search(head, x))#3
