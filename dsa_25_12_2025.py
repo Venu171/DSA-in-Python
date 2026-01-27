53c7dec3367d6f84d8f276fef62e977973338d31
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-27 08:17:57 +0530

    Implement subsequence check function
    Added a function to check if one string is a subsequence of another.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -853,3 +853,18 @@ head.next.next = Node(20)
 head.next.next.next = Node(25)
 x = 20
 print(search(head, x))#3
+#======================Check if a String is Subsequence of Other=====================================
+def issubseq(s1,s2) :
+    i, j = 0, 0
+    while(i < len(s1) and j < len(s2)) :
+        if s1[i] == s2[j] :
+            j = j + 1 
+        i += 1 
+    if j == len(s2) :
+        return True
+    else :
+        return False
+        
+s1 = "ABCDEF"
+s2 = "ADE"
+print(issubseq(s1,s2))#True
