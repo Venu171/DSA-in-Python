f332b268076f1cf224c8a685f3f79e81e4641751
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-01-29 08:53:05 +0530

    Implement leftmost repeating character function
    Added a function to find the leftmost repeating character in a string.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -891,3 +891,21 @@ Enter a String:
 uenv
 True
 """
+#===============Leftmost Repeating Character===============================
+import sys
+CHAR = 256
+def leftmost(st) :
+    findex = [-1] * CHAR
+    res = sys.maxsize
+    for i in range(len(st)) :
+        if (findex[ord(st[i])]==-1) :
+            findex[ord(st[i])] = i
+        else :
+            res = min(res,findex[ord(st[i])])
+    if res == sys.maxsize :
+        return -1
+    else :
+        return res
+        
+st = "abccbd"
+print(leftmost(st))
