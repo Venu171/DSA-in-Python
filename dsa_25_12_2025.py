9396c064b9887ca2c122423647d41e349e00c230
Venu Pelluru <85385511+Venu171@users.noreply.github.com>
2026-02-11 17:06:35 +0530

    Add minJumps function for minimum jump calculation
    Implement minJumps function to calculate minimum jumps to reach the end of the array.

--- dsa_25_12_2025.py
+++ dsa_25_12_2025.py
@@ -1072,3 +1072,22 @@ def pushZerosToEnd(arr):
     return arr
 arr = [1, 2, 0, 4, 3, 0, 5, 0]
 print(pushZerosToEnd(arr))# [1, 2, 4, 3, 5, 0, 0, 0]
+#==============================Minimum Jumps=====================================
+def minJumps(arr):
+    n = len(arr)
+
+    dp = [float('inf')] * n
+    dp[n - 1] = 0
+
+    for i in range(n - 2, -1, -1):
+        for j in range(i + 1, min(i + arr[i] + 1, n)):
+            if dp[j] != float('inf'):
+                dp[i] = min(dp[i], 1 + dp[j])
+
+    if dp[0] == float('inf'):
+        return -1
+
+    return dp[0]
+
+arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+print(minJumps(arr))#3
